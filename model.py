956c5e79dfd2cd984d58fa0730293ab49096f901
Peter Damian Vercek <peter.d.vercek@gmail.com>
2021-09-01 14:30:52 +0200

    Mentirjev log-in model del

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import json
+import hashlib
+import random
 
 def modifier(xx):
     return xx // 2 - 5
@@ -10,6 +12,79 @@ SKILLS_WIS = ['animal_handling', 'insight', 'medicine', 'perception', 'survival'
 SKILLS_INTL = ['arcana', 'history', 'investigation', 'nature', 'religion']
 SKILLS_CHA = ['deception', 'intimidation', 'performance', 'persuasion']
 SKILLS_STRG = ['athletics']
+
+
+class Uporabnik:
+    def __init__(self, uporabnisko_ime, zasifrirano_geslo, character):
+        self.uporabnisko_ime = uporabnisko_ime
+        self.zasifrirano_geslo = zasifrirano_geslo
+        self.character = character
+
+    @staticmethod
+    def prijava(uporabnisko_ime, geslo_v_cistopisu):
+        uporabnik = Uporabnik.iz_datoteke(uporabnisko_ime)
+        if uporabnik is None:
+            raise ValueError('This user does not exist')
+        elif uporabnik.prevri_geslo(geslo_v_cistopisu):
+            return uporabnik
+        else:
+            raise ValueError('The password is incorrect')
+
+    @staticmethod
+    def registracija(uporabnisko_ime, geslo_v_cistopisu):
+        if Uporabnik.iz_datoteke(uporabnisko_ime) is not None:
+            raise ValueError('This username is already occupied')
+        else:
+            zasifrirano_geslo = Uporabnik._zasifriraj_geslo(geslo_v_cistopisu)
+            uporabnik = Uporabnik(uporabnisko_ime, zasifrirano_geslo, Character())
+            uporabnik.v_datoteko()
+            return uporabnik
+
+    def _zasifriraj_geslo(geslo_v_cistopisu, sol=None):
+        if sol is None:
+            sol = str(random.getrandbits(32))
+        posoljeno_geslo = sol + geslo_v_cistopisu
+        h = hashlib.blake2b()
+        h.update(posoljeno_geslo.encode(encoding='utf-8'))
+        return f'{sol}${h.hexdigest()}'
+
+    def v_slovar(self):
+        return {
+            'uporabnisko_ime': self.uporabnisko_ime,
+            'zasifrirano_geslo': self.zasifrirano_geslo,
+            'character': self.character.prepare_to_save(),
+        }
+
+    def v_datoteko(self):
+        with open(
+            Uporabnik.ime_uporabnikove_datoteke(self.uporabnisko_ime), 'w'
+        ) as datoteka:
+            json.dump(self.v_slovar(), datoteka, ensure_ascii=False, indent=4)
+
+    def preveri_geslo(self, geslo_v_cistopisu):
+        sol, _ = self.zasifrirano_geslo.split('$')
+        return self.zasifrirano_geslo == Uporabnik._zasifriraj_geslo(geslo_v_cistopisu, sol)
+
+    @staticmethod
+    def ime_uporabnikove_datoteke(uporabnisko_ime):
+        return f'{uporabnisko_ime}.json'
+
+    @staticmethod
+    def iz_slovarja(slovar):
+        uporabnisko_ime = slovar['uporabnisko_ime']
+        zasifrirano_geslo = slovar['zasifrirano_geslo']
+        character = Character.load_character(slovar['character'])
+        return Uporabnik(uporabnisko_ime, zasifrirano_geslo, character)
+
+    @staticmethod
+    def iz_datoteke(uporabnisko_ime):
+        try:
+            with open(Uporabnik.ime_uporabnikove_datoteke(uporabnisko_ime)) as datoteka:
+                slovar = json.load(datoteka)
+                return Uporabnik.iz_slovarja(slovar)
+        except FileNotFoundError:
+            return None
+
 
 class Character:
     def __init__(self, name='', race='', subrace='', dclass='', dsubclass='', background=''):
